39ec3890fc3488a4d8eced1db8ba78bb3b43ad17
ghYoon93 <gh.yoon93@gmail.com>
2021-09-26 14:53:07 +0900

    feat: 코드 저장 기능 추가

---
+++ src/main/java/me/yghee/codesharingplatform/Code.java
@@ -0,0 +1,33 @@
+package me.yghee.codesharingplatform;
+
+public class Code {
+
+    private String date;
+
+    private String code;
+
+    public Code(String code) {
+        this.code = code;
+    }
+
+    public Code(String date, String code) {
+        this.date = date;
+        this.code = code;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
--- src/main/java/me/yghee/codesharingplatform/CodeController.java
+++ src/main/java/me/yghee/codesharingplatform/CodeController.java
@@ -1,30 +1,78 @@
 package me.yghee.codesharingplatform;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
 public class CodeController {
-    @GetMapping(value = "/code")
+    DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private Code code = new Code(LocalDateTime.now().format(FORMATTER), "");
+
+    @GetMapping(value = "/code", produces = MediaType.TEXT_HTML_VALUE)
     public String code() {
-        return "code.html";
+        return "<html>\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Code</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "  <span id=\"load_date\">"+ code.getDate() +"</span>\n" +
+                "<pre>\n" +
+                code.getCode() +
+                "</pre>\n" +
+                "</body>\n" +
+                "</html>";
+    }
+
+    @GetMapping(value = "/code/new", produces = MediaType.TEXT_HTML_VALUE)
+    public String create() {
+        return "<html>\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Create</title>\n" +
+                "    <script>\n" +
+                "      function send() {\n" +
+                "      let object = {\n" +
+                "          \"code\": document.getElementById(\"code_snippet\").value\n" +
+                "      };\n" +
+                "\n" +
+                "      let json = JSON.stringify(object);\n" +
+                "\n" +
+                "      let xhr = new XMLHttpRequest();\n" +
+                "      xhr.open(\"POST\", '/api/code/new', false)\n" +
+                "      xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');\n" +
+                "      xhr.send(json);\n" +
+                "\n" +
+                "      if (xhr.status == 200) {\n" +
+                "        alert(\"Success!\");\n" +
+                "      }\n" +
+                "}\n" +
+                "    </script>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "  <textarea id=\"code_snippet\" placeholder=\"// 코드 작성\"></textarea>\n" +
+                "  <div>" +
+                "    <button id=\"send_snippet\" type=\"submit\" onclick=\"send()\">Submit</button>\n" +
+                "  </div>" +
+                "</body>\n" +
+                "</html>";
     }
 
     @GetMapping("/api/code")
     @ResponseBody
-    public String apiCode() {
-        return "<html>\n" +
-                "<head>\n" +
-                "    <title>Code</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<pre>\n" +
-                "public static void main(String[] args) {\n" +
-                "    SpringApplication.run(CodeSharingPlatform.class, args);\n" +
-                "}</pre>\n" +
-                "</body>\n" +
-                "</html>";
+    public Code apiCode() {
+        return code;
+    }
+
+    @ResponseBody
+    @PostMapping("/api/code/new")
+    public String post(@RequestBody Code code) {
+        code.setDate(LocalDateTime.now().format(FORMATTER));
+        this.code = code;
+        return "{}";
     }
 
 }
